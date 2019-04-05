7d76a14527715ff5130d80397f58cc5c1caaa3bb
diegoveega <00148816@uca.edu.sv>
2019-04-05 15:20:02 -0600

    avance :'v

--- app/src/main/java/com/diegoveega/labo4/activities/MainActivity.kt
+++ app/src/main/java/com/diegoveega/labo4/activities/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.diegoveega.labo4.activities
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.diegoveega.labo4.R
 import com.diegoveega.labo4.adapters.MovieAdapter
+import com.diegoveega.labo4.network.NetworkUtils
 import com.diegoveega.labo4.pojos.Movie
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initRecyclerView()
-
+        initSearchButton()
 
     }
 
@@ -31,5 +37,50 @@ class MainActivity : AppCompatActivity() {
 
         movieAdapter = MovieAdapter(movieList, { movieItem: Movie -> movieItemClicked(movieItem) })
 
+        movie_list_rv.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = movieAdapter
+        }
+
     }
+
+    fun initSearchButton() = add_movie_btn.setOnClickListener {
+        if( !movie_name_et.text.toString().isEmpty()){
+            FetchMovie().execute(movie_name_et.text.toString())
+        }
+    }
+
+    private inner class FetchMovie : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+
+            if(params.isNullOrEmpty()) return ""
+
+            val movieName = params[0]
+
+            val movieUrl = NetworkUtils().buildtSearchUrl(movieName)
+
+            return try {
+                NetworkUtils().getResponseFromHttpUrl(movieUrl)
+            } catch (e: IOException) {
+                ""
+            }
+        }
+
+        override fun onPostExecute(movieInfo: String?) {
+            super.onPostExecute(movieInfo)
+            if(!movieInfo.isEmpty()){
+                val movieJson = JSONObject(movieInfo)
+                if (movieJson.getString("Response") == "True") {
+                    val movie = Gson().fromJson<Movie>(movieInfo, Movie::class.java)
+                    addMovieToList(movie)
+                } else {
+                    Snackbar.make(main_ll, "No existe la película en la base", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
 }
